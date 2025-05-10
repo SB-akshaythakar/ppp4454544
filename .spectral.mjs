8719a08eeff88801bdbad0965798f9350507aea0
sdsds <akshay.thakar@smartbear.com>
2025-05-10 09:35:21 +0530

    Added 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI4MDgyOQ/spectral.js?branch_slug=main&token=ce823b22-f982-42cf-b030-5ad8df2cc0b2";
+export default { extends: ruleset };
